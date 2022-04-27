1c55da921970c244dce3d6adeb691901fa84df03
Frederik Fink Højriis <frfh@itu.dk>
2022-04-27 12:06:06 +0200

    cleanup

--- pages/collection/[collectionID]/index.js
+++ pages/collection/[collectionID]/index.js
@@ -1,16 +1,44 @@
 
 // Importing useRouter()
 import { useRouter } from 'next/router'
+import React, { useState, useEffect } from "react";
+import TmpTable from '../../../components/TmpTable';
 
 const All = () => {
 
     // Initializing useRouter()
     const router = useRouter()
+    const collectionID = router.query.collectionID
+
+    console.log(collectionID);
+
+    const [data, setData] = useState([]);
+
+    const fetchData = async () => {
+    //   try {
+    //     const response = await fetch('http://127.0.0.1:5000/collection/all', {
+    //       method: 'GET',
+    //       headers: {
+    //         'Content-Type': 'application/json'
+    //       }
+    //     });
+  
+    //     setData(await response.json(data));
+  
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    };
+  
+    useEffect(() => {
+      fetchData();
+    }, []);
 
     return (
-        <p>
+        <>
             alle tokens for collection listes her
-        </p>
+            <TmpTable />
+        </>
     );
 }
 
